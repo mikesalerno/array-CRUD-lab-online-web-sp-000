16c576e6f23f6020e77d841e2efb680b508ac2f9
Michael <wtfdic@hotmail.com>
2020-11-22 02:32:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array << "last"
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array.unshift("first")
 end
 
 def remove_element_from_end_of_array(array)
@@ -23,7 +23,6 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[1]
 end
 
 def retrieve_first_element_from_array(array)
